e4df03b40d9a14e6157f92b82767f4ab8259869d
jossie0280 <acostajossieca@gmail.com>
2025-11-29 21:21:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let intentos = 1;
 
 const inputUsuario = document.getElementById('entradaUsuario');
 const botonIntentar = document.getElementById('botonIntentar');
+const botonReiniciar = document.getElementById('botonReiniciar');
 const pMensaje = document.getElementById('mensaje');
 
 function verificarIntento() {
@@ -19,7 +20,9 @@ function verificarIntento() {
         let palabraIntento = intentos === 1 ? "intento" : "intentos";
         pMensaje.style.color = "#bb86fc";
         pMensaje.innerHTML = `¡Felicidades! Adivinaste el número en ${intentos} ${palabraIntento}.`;
+        
         botonIntentar.setAttribute('disabled', 'true');
+        botonReiniciar.removeAttribute('disabled');
     } else {
         if (numeroSecreto > numeroUsuario) {
             pMensaje.textContent = "El número secreto es mayor.";
@@ -36,4 +39,16 @@ function limpiarCaja() {
     inputUsuario.focus();
 }
 
+function reiniciarJuego() {
+    numeroSecreto = Math.floor(Math.random() * 10) + 1;
+    intentos = 1;
+    
+    limpiarCaja();
+    pMensaje.textContent = '';
+    
+    botonIntentar.removeAttribute('disabled');
+    botonReiniciar.setAttribute('disabled', 'true');
+}
+
 botonIntentar.addEventListener('click', verificarIntento);
+botonReiniciar.addEventListener('click', reiniciarJuego);
